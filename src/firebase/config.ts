21c3b09dea55facc2a01a25fd83beedf48f18ac4
moisés Bueno <moisesbueno00092@gmail.com>
2025-12-19 11:25:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5553654367-98546",
+  "appId": "1:374860758714:web:09c2e02299b4f7df584c83",
+  "apiKey": "AIzaSyCYm_FaDBY708Fn8IH-Oz3JuPdy1qv-47Q",
+  "authDomain": "studio-5553654367-98546.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "374860758714"
+};
