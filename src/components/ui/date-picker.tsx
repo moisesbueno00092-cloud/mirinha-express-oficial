4ae611bfcf7a77db9682d420d1f8b33f71b11100
moisés Bueno <moisesbueno00092@gmail.com>
2025-12-27 18:43:27 +0000

    retire os nomes dos dias da tada

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -4,7 +4,6 @@
 import * as React from "react"
 import { format } from "date-fns"
 import { ptBR } from 'date-fns/locale';
-import { Calendar as CalendarIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
@@ -31,7 +30,7 @@ export function DatePicker({ date, setDate }: DatePickerProps) {
             !date && "text-muted-foreground"
           )}
         >
-          {date ? format(date, "PPP", { locale: ptBR }) : <span>Selecione uma data</span>}
+          {date ? format(date, "P", { locale: ptBR }) : <span>Selecione uma data</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
