f36ad9e79e7e7e265a8dee366f69854d744d572d
moisés Bueno <moisesbueno00092@gmail.com>
2025-12-28 23:05:24 +0000

    Try fixing this error: `Runtime ReferenceError: useEffect is not defined

--- src/components/admin/funcionarios-panel.tsx
+++ src/components/admin/funcionarios-panel.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { useCollection, useFirestore, useMemoFirebase } from '@/firebase';
 import { collection, query, orderBy, doc } from 'firebase/firestore';
 import { useToast } from '@/hooks/use-toast';
