7ef50b613d43499da337aab3d524d3d0e66601b3
moisés Bueno <moisesbueno00092@gmail.com>
2026-01-03 22:48:19 +0000

    n

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -41,7 +41,7 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] flex items-center justify-center",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
@@ -58,6 +58,13 @@ function Calendar({
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
+      }}
+      formatters={{
+        formatWeekdayName: (day, options) => (
+            <div className="flex h-9 w-9 items-center justify-center p-0">
+                {format(day, 'eee', { locale: options?.locale }).slice(0, 3)}
+            </div>
+        )
       }}
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
